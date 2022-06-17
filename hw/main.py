3c2d48ebe3abb705e0b16a4abff090bbefde0eff
SultanEsen <106883806+SultanEsen@users.noreply.github.com>
2022-06-17 16:18:18 +0600

    Delete hw directory

--- hw/main.py
+++
@@ -1,8 +0,0 @@
-from cars.create_car import create_car
-from cars.get_car_info import get_car_info
-
-bmw = create_car('BMW', 'X7', 2020, 5, 20000)
-bmw.start()
-bmw.stop()
-
-mers = get_car_info('Mers', 'W140', 1995, 6, 500000)
